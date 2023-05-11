51a19dfb1cd78be16d4a7a15457786b4a9cb57ce
David Rodriguez <davrdz25@yahoo.com.mx>
2023-05-10 18:59:19 -0600

    QRGenertion Searcj implementation

--- Models/QRCode.ts
+++ Models/QRCode.ts
@@ -3,54 +3,50 @@ import IQRCode from "../Interfaces/QRCode"
 import MSSQLService from "../Services/Database"
 import QRCode from "qrcode"
 
-export default class QRCodeModel{
-    public static Get(_Entry: number){
+export default class QRCodeModel {
+    public static Get(_Entry: number) {
         const SQLQuery = "SELECT ImagePath FROM QRCodes WHERE \"Entry\" = " + _Entry
 
         return new Promise((resolve, reject) => {
             MSSQLService.RunQuey(SQLQuery).then((_Created: IResult<any>) => {
                 if (_Created.recordset[0].ErrNumber === 500) {
-                        reject({ Message: _Created.recordset })
-                    }
+                    reject({ Message: _Created.recordset })
+                }
                 resolve(_Created.recordset)
-                }).catch((_Err) => {
-                reject({"Message": _Err.Info.message})
+            }).catch((_Err) => {
+                reject({ "Message": _Err.Info.message })
             })
         })
     }
 
-    public static Create(_NewQRCode: IQRCode){
-
-
-    return new Promise((resolve, reject) => {
-        QRCode.toFile(".sad/" + _NewQRCode.Name + ".png" ,"www.google.com").then((w) => {
-            console.log(w)
-
-            const SQLQuery = "INSERT INTO QRCodes (" +
-                         "\"Entry\", " +
-                         "\"Name\", " +
-                         "\"Data\", " +
-                         "UserSign, " +
-                         "CreateDate) " +
-                         "VALUES (" +
-                         "(SELECT ISNULL(MAX(\"Entry\"), 0) + 1 \"Entry\" FROM QRCodes) , " +
-                         "'" + _NewQRCode.Name + "', " +
-                         "'" + -_NewQRCode.Data + "', " +
-                         -_NewQRCode.UserSign + ", " +
-                         "'" + _NewQRCode.CreateDate + "'" + 
-                         ")"
-                         console.log(SQLQuery)
-            MSSQLService.RunQuey(SQLQuery).then((_Created: IResult<any>) => {
-                if (_Created.recordset[0].ErrNumber === 500) {
+    public static Create(_NewQRCode: IQRCode) {
+        return new Promise((resolve, reject) => {
+            QRCode.toFile(".sad/" + _NewQRCode.Name + ".png", "www.google.com").then((w) => {
+                const SQLQuery = "INSERT INTO QRCodes (" +
+                    "\"Entry\", " +
+                    "\"Name\", " +
+                    "\"Data\", " +
+                    "UserSign, " +
+                    "CreateDate) " +
+                    "VALUES (" +
+                    "(SELECT ISNULL(MAX(\"Entry\"), 0) + 1 \"Entry\" FROM QRCodes) , " +
+                    "'" + _NewQRCode.Name + "', " +
+                    "'" + -_NewQRCode.Data + "', " +
+                    -_NewQRCode.UserSign + ", " +
+                    "'" + _NewQRCode.CreateDate + "'" +
+                    ")"
+                    
+                MSSQLService.RunQuey(SQLQuery).then((_Created: IResult<any>) => {
+                    if (_Created.recordset[0].ErrNumber === 500) {
                         reject({ Message: _Created.recordset })
                     }
-                resolve(_Created.recordset)
+                    resolve(_Created.recordset)
                 }).catch((_Err) => {
-                reject({"Message": _Err.Info.message})
+                    reject({ "Message": _Err.Info.message })
+                })
+            }).catch((err) => {
+                reject(err)
             })
-        }).catch((err) => {
-            reject({"Message": err})
         })
-    })
     }
 }
--- Routes/QRGenerator.ts
+++ Routes/QRGenerator.ts
@@ -1,10 +1,18 @@
 import { QRCode } from "qrcode";
-import { Body, Get, JsonController, Post, Res } from "routing-controllers";
+import { Body, Get, JsonController, Post, QueryParam, Res } from "routing-controllers";
 import QRCodeModel from "../Models/QRCode";
 import IQRCode from "../Interfaces/QRCode";
 
 @JsonController("QRCode")
 export default class QRGenerator {
+    @Get()
+    public Search(@Res() response: any){
+        return QRCodeModel.Get(1).then((_QR) => {
+            return response.status(200).send(_QR)
+        }).catch((err) => {
+            return response.status(500).send(err)
+        })
+    }
 
     @Post()
     public Add(@Body({required: true}) NewQRCode: IQRCode, @Res() response: any){
