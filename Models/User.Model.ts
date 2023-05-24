56ed31597a159ee2fed7e0db11de71ee72d9f952
David Rodriguez <davrdz25@yahoo.com.mx>
2023-05-23 21:01:20 -0600

    Added Email property

--- Models/User.Model.ts
+++ Models/User.Model.ts
@@ -77,6 +77,7 @@ export default class UserModel {
                         const SQLQuery = `SELECT "Entry",
                                          "Code",
                                          "Name",
+                                         Email,
                                          Comments,
                                          isLocked,
                                          isActive,
