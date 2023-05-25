32874ccd9d128bc7dc294ecb0e628ff5e46307cd
David Rodriguez <davrdz25@yahoo.com.mx>
2023-05-24 21:28:33 -0600

    Fix Encrytp to return strings

--- Services/Encyption.ts
+++ Services/Encyption.ts
@@ -4,21 +4,20 @@ export default class Encryption {
      
     public static EncryptString(_String: string){
         const iv = randomBytes(16);
-
+        
         let cipher = createCipheriv("aes-256-cbc", ( "<==>" + process.env.PasswordHash + "<==>"), iv);
         let encrypted = cipher.update(_String);
 
         encrypted = Buffer.concat([encrypted, cipher.final()]);
 
-        return {
-            iv: iv.toString('hex'),
-            encryptedData: encrypted.toString('hex') 
-        };
+        return iv.toString('hex') + ".:." + encrypted.toString('hex') 
     }
 
     public static DecryptString(_String: any){
-        let iv = Buffer.from(_String.iv, 'hex');
-        let encryptedText = Buffer.from(_String.encryptedData, 'hex');
+        const data = _String.split(".:.")
+
+        let iv = Buffer.from(data.shift(), 'hex');
+        let encryptedText = Buffer.from(data.join(':'), 'hex');
         let decipher = createCipheriv("aes-256-cbc", ( "<==>" + process.env.PasswordHash + "<==>"), iv);
  
         let decrypted = decipher.update(encryptedText);
