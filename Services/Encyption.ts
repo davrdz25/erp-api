604f2b373d6a978b0588613cbcd6e9c653b37b2d
David Rodriguez <davrdz25@yahoo.com.mx>
2023-05-24 21:11:40 -0600

    Fix encryption

--- Services/Encyption.ts
+++ Services/Encyption.ts
@@ -1,23 +1,25 @@
 import { createCipheriv, randomBytes } from "crypto";
 
 export default class Encryption {
-    private static key = randomBytes(32);
-    private static iv = randomBytes(16);
      
     public static EncryptString(_String: string){
-        let cipher = createCipheriv("aes-256-cbc", Buffer.from(this.key), this.iv);
+        const iv = randomBytes(16);
+
+        let cipher = createCipheriv("aes-256-cbc", ( "<==>" + process.env.PasswordHash + "<==>"), iv);
         let encrypted = cipher.update(_String);
 
         encrypted = Buffer.concat([encrypted, cipher.final()]);
 
-        return { iv: this.iv.toString('hex'),
-        encryptedData: encrypted.toString('hex') };
+        return {
+            iv: iv.toString('hex'),
+            encryptedData: encrypted.toString('hex') 
+        };
     }
 
     public static DecryptString(_String: any){
         let iv = Buffer.from(_String.iv, 'hex');
         let encryptedText = Buffer.from(_String.encryptedData, 'hex');
-        let decipher = createCipheriv("aes-256-cbc", Buffer.from(this.key), iv);
+        let decipher = createCipheriv("aes-256-cbc", ( "<==>" + process.env.PasswordHash + "<==>"), iv);
  
         let decrypted = decipher.update(encryptedText);
         decrypted = Buffer.concat([decrypted, decipher.final()]);
